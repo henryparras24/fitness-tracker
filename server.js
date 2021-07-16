159098af4abab1cd7a28b0ae4c6a82715dcff06b
Henry Parras <henry.parras@yahoo.com>
2021-07-15 21:02:14 -0700

    2 routes are working

--- server.js
+++ server.js
@@ -15,46 +15,50 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-db.Library.create({ name: "Campus Library" })
-  .then(dbLibrary => {
-    console.log(dbLibrary);
-  })
-  .catch(({message}) => {
-    console.log(message);
-  });
+// db.Library.create({ name: "Campus Library" })
+//   .then(dbLibrary => {
+//     console.log(dbLibrary);
+//   })
+//   .catch(({message}) => {
+//     console.log(message);
+//   });
 
+
+// getting all the workouts
+app.get("/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// get a single workout
+app.get("/workouts/:id", (req, res) => {
+  db.Workout.findOne({id: req.body.id})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// update a workout POSSIBLE
 app.post("/submit", ({body}, res) => {
-  db.Book.create(body)
-    .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
-    .then(dbLibrary => {
-      res.json(dbLibrary);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get("/books", (req, res) => {
-  db.Book.find({})
-    .then(dbBook => {
-      res.json(dbBook);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get("/library", (req, res) => {
-  db.Library.find({})
-    .then(dbLibrary => {
-      res.json(dbLibrary);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+    db.Book.create(body)
+      .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
+      .then(dbLibrary => {
+        res.json(dbLibrary);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 
 app.get("/populated", (req, res) => {
   db.Library.find({})
